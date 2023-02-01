f2d424b7d651fa8224f7ec7f2d843e4667852605
Sherkhan K <sherkhan4work@gmail.com>
2023-02-01 17:59:07 +0600

    If we want to seed our database, we can call this But careful, it deletes all in DB, and replaces with new ones

---
+++ seeds/index.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.set('strictQuery', true);
+main().catch(err => console.log(err));
+
+async function main() {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+        console.log('MONGO CONNECTION OPEN');
+    } catch (err) {
+        console.log('OH NO, MONGO ERORRRRR!');
+        console.log(err);
+    }
+}
+
+const sample = array => array[Math.floor(Math.random() * array.length)]
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    // const c = new Campground({title: 'purple field'});
+    // await c.save();
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        await camp.save()
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+});
