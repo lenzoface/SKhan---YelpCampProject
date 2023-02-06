5a87d342f6762fae8a1b516f73ab1b529c3f7941
Sherkhan K <sherkhan4work@gmail.com>
2023-02-06 23:40:47 +0600

    Added reviews (create, show, delete) + delete reviews connected to a camp that is being deleted, using  middleware

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
 const mongoose = require('mongoose');
-const ejsMate = require('ejs-mate'); 
-const {campgroundSchema} = require('./schemas.js') //Joi validation (npm)
-const catchAsync = require('./utils/catchAsync')
-const ExpressError = require('./utils/ExpressError')
-const methodOverride = require('method-override')
-const Campground = require('./models/campground')
+const ejsMate = require('ejs-mate');
+const { campgroundSchema, reviewSchema } = require('./schemas.js'); //Joi validation (npm)
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
+const methodOverride = require('method-override');
+const Campground = require('./models/campground');
+const Review = require('./models/review');
 
 
 mongoose.set('strictQuery', true);
@@ -33,8 +34,17 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'));
 
-const validateCampground = (req,res,next)=>{
+const validateCampground = (req, res, next) => {
     const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
     if (error) {
         const msg = error.details.map(el => el.message).join(',');
         throw new ExpressError(msg, 400)
@@ -65,7 +75,7 @@ app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) =
 }))
 
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
+    const campground = await Campground.findById(req.params.id).populate('reviews');
     res.render('campgrounds/show', { campground })
 }))
 
@@ -84,6 +94,24 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}))
+
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+}))
+
+
+// reviews under camp will be deleted too, using this delete middleware, stated in campground.js document
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
 }))
 
 app.all('*', (req, res, next) => {
