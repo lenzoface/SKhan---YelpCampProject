41db34dbf88e2310ebb8721b3a091f78b65e37d0
Sherkhan K <sherkhan4work@gmail.com>
2023-02-11 00:41:48 +0600

    changed map style to outdoors

--- public/javascripts/showPageMaps.js
+++ public/javascripts/showPageMaps.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = mapToken; //passed from script in show.ejs file
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    style: 'mapbox://styles/mapbox/outdoors-v11', // style URL
     center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 8, // starting zoom
 });
